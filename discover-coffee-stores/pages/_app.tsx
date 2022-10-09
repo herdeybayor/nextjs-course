319eaf0293bd8f453016faa5530faff78e3077ac
Sherifdeen Adebayo <sherifdeen.adebayo@leftlanetech.co>
2022-10-09 11:18:07 +0100

    feat: add app background 🎉

--- discover-coffee-stores/pages/_app.tsx
+++ discover-coffee-stores/pages/_app.tsx
@@ -1,14 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Footer } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Component {...pageProps} />
-            <Footer />
-        </>
-    );
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
